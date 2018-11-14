04ec3c3dc621702c647d443d1cab7eea8ffb4317
Knapstad <b.knapsta@gmail.com>
2018-11-14 14:15:25 +0100

    Hurtigprisbereger

---
+++ hurtigprisBeregner/hurtigprisBeregner.js
@@ -0,0 +1,22 @@
+try {
+    document.querySelector('[src="/forsikring/2.7439"]').contentWindow.document.querySelector('[id="bilfor_submit"]').onclick = function() {
+      utag.link({
+        success: "hurtigpris",
+        event_category: "Forsikring",
+        event_action: "Click",
+        event_label: "hurtigpris",
+        event_value: "1",
+      });
+    };
+    document.querySelector('[src="/forsikring/2.7439"]').contentWindow.document.querySelector('[id="bilfor_submit_mobile"]').onclick = function() {
+      utag.link({
+        success: "hurtigpris_mobil",
+        event_category: "Forsikring",
+        event_action: "Click",
+        event_label: "hurtigpris_mobil",
+        event_value: "1",
+      });
+    };
+  } catch (error) {
+    utag.DB("Error UID 100 " + "(" + error.name + ": " + error.message + ")");
+  }
