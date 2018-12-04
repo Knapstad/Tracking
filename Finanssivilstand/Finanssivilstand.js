6938dfb502573e962f094e9f2112cbcf7b5fa593
Knapstad <b.knapsta@gmail.com>
2018-12-04 14:54:04 +0100

    add kill onbeforeunload

--- Finanssivilstand/Finanssivilstand.js
+++ Finanssivilstand/Finanssivilstand.js
@@ -1,16 +1,30 @@
 window.onbeforeunload = sendSivilstatus;
   function sendSivilstatus()
   {
+    if(document.querySelector(".klp-bg--alpha-darker")===null){
+    var sivilstatus = document.querySelector("[label='Sivilstatus']").value
+  }
+  else
+  {
+   var sivilstatus =document.querySelector(".klp-bg--alpha-darker").textContent+document.querySelector("[label='Sivilstatus']").value
+  }
     utag.link({
-        "sivilstatus" : document.querySelector("[label='Sivilstatus']").value})
+      event_category: "Closewindow",
+      event_action: sivilstatus,
+      event_label: document.querySelector("[label='Sivilstatus']").value,
+      event_value: 0,
+    })
   }
 
 
 
 var nesteKnapp = document.querySelectorAll("button")[1]
-nesteKnapp.addEventListener("click", function(){
-    utag.link({
-    "sivilstatus" : document.querySelector("[label='Sivilstatus']").value});
-     function confirmExit() {}})
+nesteKnapp.addEventListener("click", function(){sendSivilstatus(); killOnbeforeunload();})
+
+function killOnbeforeunload(){
+  setTimeout(50)
+  if(document.querySelector(".klp-summary-currentstep").textContent!=="1. Låntakere"){window.onbeforeunload=null}
+}
+
 
 
