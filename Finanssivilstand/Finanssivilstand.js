39f5709cb5f477a66af0069464d33b81a2ad6c88
Knapstad <b.knapsta@gmail.com>
2018-12-05 09:40:33 +0100

    add debug

--- Finanssivilstand/Finanssivilstand.js
+++ Finanssivilstand/Finanssivilstand.js
@@ -1,30 +1,44 @@
-window.onbeforeunload = sendSivilstatus;
-  function sendSivilstatus()
-  {
-    if(document.querySelector(".klp-bg--alpha-darker")===null){
-    var sivilstatus = document.querySelector("[label='Sivilstatus']").value
-  }
-  else
-  {
-   var sivilstatus =document.querySelector(".klp-bg--alpha-darker").textContent+document.querySelector("[label='Sivilstatus']").value
-  }
-    utag.link({
-      event_category: "Closewindow",
-      event_action: sivilstatus,
-      event_label: document.querySelector("[label='Sivilstatus']").value,
-      event_value: 0,
-    })
-  }
-
-
-
-var nesteKnapp = document.querySelectorAll("button")[1]
-nesteKnapp.addEventListener("click", function(){sendSivilstatus(); killOnbeforeunload();})
-
 function killOnbeforeunload(){
-  setTimeout(50)
+  setTimeout(500);
   if(document.querySelector(".klp-summary-currentstep").textContent!=="1. Låntakere"){window.onbeforeunload=null}
 }
 
+function addNextButtonEvent(){
+nesteKnapp = document.querySelectorAll("button")[1];
+nesteKnapp.addEventListener("click", function(){sendSivilstatus(); killOnbeforeunload();});}
 
 
+function sendSivilstatus()
+  {
+    if(document.querySelector(".klp-bg--alpha-darker")===null){
+     sivilstatus = document.querySelector("[label='Sivilstatus']").value;
+  }
+  else
+  {
+    sivilstatus =document.querySelector(".klp-bg--alpha-darker").textContent+document.querySelector("[label='Sivilstatus']").value;
+  }
+    utag.link({
+      
+      "sivilstatus": sivilstatus,
+      
+    });
+  }
+ 
+try{
+window.onbeforeunload = sendSivilstatus;
+}
+catch(error){
+    utag.DB("Error UID 156 onbeforeunload ("+ error.name+ ":" + error.message +")");
+    
+}
+
+try{
+    waitForElement=  window.setInterval(function(){
+        if(document.readyState=== "complete"){
+            addNextButtonEvent();
+            clearInterval(waitForElement)}}, 500);
+}
+
+catch(error){
+    utag.DB("Error UID 156 onclick ("+ error.name+ ":" + error.message +")");
+  }
