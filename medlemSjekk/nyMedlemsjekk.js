3f9d85ab1a199b4044c1b81c9cce36f07e160388
Knapstad <b.knapsta@gmail.com>
2018-12-05 15:25:02 +0100

    ny meldemsjekk virker

--- medlemSjekk/nyMedlemsjekk.js
+++ medlemSjekk/nyMedlemsjekk.js
@@ -1,37 +1,40 @@
-
-
-function checkState(interval){
-let style_medlem = document.querySelector('[src="/medlemsfordeler/medlemsjekk-hvit"]').contentWindow.document.querySelector('[id="isMember"]').style.display;
-let style_ikkemedlem = document.querySelector('[src="/medlemsfordeler/medlemsjekk-hvit"]').contentWindow.document.querySelector('[id="isNotMember"]').style.display;
-
-if(style_medlem !== "none"){
-            utag.link({
-            success: "medlemssjekk-medlem",
-            event_category: "Medlem",
-            event_action: "Click",
-            event_label: "Medlem",
-            event_value: "1",
-            relationship: "medlem",
-          });
-        clearInterval(medlemsjekk)
-    };
-if(style_ikkemedlem !== "none"){
+function checkState(){
+    var style_medlem = document.querySelector('[src="/medlemsfordeler/medlemsjekk-hvit"]').contentWindow.document.querySelector('[id="isMember"]').style.display;
+    var style_ikkemedlem = document.querySelector('[src="/medlemsfordeler/medlemsjekk-hvit"]').contentWindow.document.querySelector('[id="isNotMember"]').style.display;
     
-        utag.link({
-            success: "medlemssjekk-ikkemedlem",
-            event_category: "Medlem",
-            event_action: "Click",
-            event_label: "Ikkemedlem",
-            event_value: "1",
-            relationship: "ikkemedlem",
-            });
-        clearInterval(medlemsjekk);
+    if(style_medlem !== "none"){
+            
+                utag.link({
+                success: "medlemssjekk-medlem",
+                event_category: "Medlem",
+                event_action: "Click",
+                event_label: "Medlem",
+                event_value: "1",
+                relationship: "medlem",
+              });
+            clearInterval(medlemsjekk)
+        }
+    if(style_ikkemedlem !== "none"){
+            
+        
+            utag.link({
+                success: "medlemssjekk-ikkemedlem",
+                event_category: "Medlem",
+                event_action: "Click",
+                event_label: "Ikkemedlem",
+                event_value: "1",
+                relationship: "ikkemedlem",
+                });
+            clearInterval(medlemsjekk);
+        }
     }
-}
-waitDom=  setInterval(function(){
-   if (document.readyState == "complete"){
-    medlemsjekk = setInterval(checkState, 500)
-    clearInterval(waitDom)}
-   } 
-, 500)
-
+    waitDom =  setInterval(function(){
+       if (document.readyState == "complete"){
+        console.log("clearing waitDom")
+        medlemsjekk = setInterval(checkState, 500)
+        clearInterval(waitDom)}
+       } 
+    , 500)
+    
+    
+    
