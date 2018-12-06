5cf995df34086525401a857aeea51d56f037789c
Knapstad <b.knapsta@gmail.com>
2018-12-06 15:06:38 +0100

    ny personforsikring

--- beregnPersonForsikring/beregnPersonForsikring.js
+++ beregnPersonForsikring/beregnPersonForsikring.js
@@ -1,14 +1,20 @@
-try {
-  document.querySelector('[src="/forsikring/personforsikringer/forsikringsveileder/forsikringsveileder-plugin"]').contentWindow.document.querySelector('button').onclick = function() {
-    utag.link({
-      success: "beregnpersonforsikrig",
-      event_category: "Forsikring",
-      event_action: "Click",
-      event_label: "beregnpersonforsikrig",
-      event_value: "1",
-    });
-  };
+waitDom151 = setInterval(function(){
+  if(document.readyState==="complete" && document.querySelector('[src="/forsikring/personforsikringer/forsikringsveileder/forsikringsveileder-plugin"]').contentWindow.document.readyState == "complete"){
+  setTimeout(function(){
+  try {
+      document.readyState==="complete" && document.querySelector('[src="/forsikring/personforsikringer/forsikringsveileder/forsikringsveileder-plugin"]').contentWindow.document.readyState == "complete";
+      document.querySelector('[src="/forsikring/personforsikringer/forsikringsveileder/forsikringsveileder-plugin"]').contentWindow.document.querySelector('button').addEventListener("click", 
+      ()=>{utag.link({
+        success: "beregnpersonforsikrig",
+        event_category: "Forsikring",
+        event_action: "Click",
+        event_label: "beregnpersonforsikrig",
+        event_value: "1",
+      })}
+    )}
+      
+   catch (error) {
+    utag.DB("Error UID 151 " + "(" + error.name + ": " + error.message + ")");
     
-} catch (error) {
-  utag.DB("Error UID 151 " + "(" + error.name + ": " + error.message + ")");
-}
+  }},500)
+  ;clearInterval(waitDom151)}},500)
