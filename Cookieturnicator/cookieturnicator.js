29a93ddc7d1e715cd19080968f18bbac6e7bb84d
Knapstad <b.knapsta@gmail.com>
2019-01-25 10:20:35 +0100

    cookieturnicator done

--- Cookieturnicator/cookieturnicator.js
+++ Cookieturnicator/cookieturnicator.js
@@ -1,16 +1,16 @@
 function smaakaker(){
 let smaakaker = document.cookie.split(";");
-for(i=0; i<smaakaker.length+1; i++){
+for(i=0; i<smaakaker.length; i++){
     if(smaakaker[i].length > 1000){
         let nykake = smaakaker[i].trim();
         let kakestart=nykake.slice(0,nykake.indexOf("=")+1);
         if (kakestart.includes("channel")){
             nykake = nykake.slice(nykake.indexOf(",")+1);
-            while(nykake.length > 1000){
+            while(nykake.length > 1020){
                 nykake = nykake.slice(nykake.indexOf(",")+1);
             }
     
-            nykake = kakestart+nykake+";domain=.klp;max-age=5184000";
+            nykake = kakestart+nykake+";domain=.klp.no;max-age=5184000";
             document.cookie = nykake;
 
         }
