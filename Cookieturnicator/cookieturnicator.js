4dbf2bdad7b9a5495eaa178453ae215cf2b123a8
Knapstad <b.knapsta@gmail.com>
2019-01-25 08:56:19 +0100

    cookieturnicator done

--- Cookieturnicator/cookieturnicator.js
+++ Cookieturnicator/cookieturnicator.js
@@ -1,12 +1,20 @@
-function kjeks(){
-let smaakaker = document.cookie.split(";")
-for(i=0; i>=smaakaker.length; i++){
-    if(smaakaker[i].length > 400){
-        let nykake = smaakaker[i].trim()
-        let kakestart=nykake.slice(0,nykake.indexOf("=")+1)
-        nykake = nykake.slice(nykake.indexOf(",")+1)
-        nykake = kakestart+nykake+";max-age=5184000"
-        document.cookie = nykake
+function smaakaker(){
+let smaakaker = document.cookie.split(";");
+for(i=0; i<smaakaker.length+1; i++){
+    if(smaakaker[i].length > 1000){
+        let nykake = smaakaker[i].trim();
+        let kakestart=nykake.slice(0,nykake.indexOf("=")+1);
+        if (kakestart.includes("channel")){
+            nykake = nykake.slice(nykake.indexOf(",")+1);
+            while(nykake.length > 1000){
+                nykake = nykake.slice(nykake.indexOf(",")+1);
+            }
+    
+            nykake = kakestart+nykake+";domain=.klp;max-age=5184000";
+            document.cookie = nykake;
+
+        }
+        
     }
 }
 }
